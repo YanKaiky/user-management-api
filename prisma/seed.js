57171d89bb2a6b91dd5b5369ae197b9cf984a4a8
22-YanKaiky <yankaikys@gmail.com>
2022-08-27 16:38:14 -0300

    feat(prisma): add seed

---
+++ prisma/seed.js
@@ -0,0 +1,78 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function seed() {
+  const citiesData = [
+    {
+      name: 'Blumenau',
+      uf: 'SC',
+    },
+    {
+      name: 'New York',
+      uf: 'NY',
+    },
+    {
+      name: 'Parnarama',
+      uf: 'MA',
+    },
+    {
+      name: 'São Paulo',
+      uf: 'SP',
+    },
+  ]
+
+
+  for (const city of citiesData) {
+    await prisma.cities.create({
+      data: city
+    });
+  }
+
+  const people = [
+    {
+      name: 'Yan Kaiky',
+      last_name: 'Augusto dos Santos',
+      email: 'yankaikys@gmail.com',
+      cpf: '13424551937',
+      birth_date: new Date('2002-12-22T23:15'),
+    },
+    {
+      name: 'Miles',
+      last_name: 'Morales',
+      email: 'miles@gmail.com',
+      cpf: '57520593053',
+      birth_date: new Date('2000-12-22T00:00'),
+    },
+    {
+      name: 'Gwen',
+      last_name: 'Stacy',
+      email: 'gwen@gmail.com',
+      cpf: '26965804050',
+      birth_date: new Date('2001-11-21T00:00'),
+    },
+    {
+      name: 'Peter',
+      last_name: 'Park',
+      email: 'peter@gmail.com',
+      cpf: '08197375003',
+      birth_date: new Date('2002-11-12T00:00'),
+    },
+  ]
+
+  const city = await prisma.cities.findFirst({
+    where: {
+      name: 'Blumenau'
+    }
+  })
+
+  for (const person of people) {
+    await prisma.peoples.create({
+      data: {
+        ...person,
+        city_guid: city.guid,
+      }
+    })
+  };
+}
+
+seed();
--- src/app/services/people.service.js
+++ src/app/services/people.service.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 const { PrismaClient } = require("@prisma/client");
 const createError = require("http-errors");
+const CitiesService = require("./cities.service");
 const prisma = new PrismaClient();
 
 class PeopleService {
   static createPeople = async (payload) => {
     payload.birth_date = new Date(payload.birth_date).toISOString()
-    
+
+    const city = await CitiesService.getCityByGuid(payload.city_guid);
+
+    if (!city) throw createError.NotFound("CITY_NOT_FOUND");
+
     const people = await prisma.peoples.create({ data: payload });
 
     return people;
@@ -40,6 +45,12 @@ class PeopleService {
     if (!people) throw createError.NotFound("PEOPLE_NOT_FOUND");
 
     if (payload.birth_date) payload.birth_date = new Date(payload.birth_date).toISOString()
+
+    if (payload.city_guid) {
+      const city = await CitiesService.getCityByGuid(payload.city_guid);
+
+      if (!city) throw createError.NotFound("CITY_NOT_FOUND");
+    }
 
     const updatePeople = await prisma.peoples.update({
       where: {
